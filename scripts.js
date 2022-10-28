5c2835b67c7fe97208c9825fe7d477e5c78917c9
Elhathout_Youcode_Youss <79522043+elhathoute@users.noreply.github.com>
2022-10-29 00:54:46 +0100

    i get the id and title but i have error in date

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
-function edit(id){
-    document.getElementById("task-id").value=id;
-    
-    }
+function edit(id) {
+    //get the id from the button
+    document.getElementById("task-id").value = id;
+    //get the title 
+    document.getElementById("task-title").value = document.getElementById(id).children[1].children[0].innerHTML;
+    //get the date
+    //error in datetime task
+    document.getElementById("task-date").value = document.getElementById(id).children[1].children[1].children[0].children[1].innerHTM;
+}
